f8f086b142749b076f0ca417902a102e0f68c2e4
John Nair <jaibaps1991@gmail.com>
2024-10-26 19:59:51 +0530

    custom Render in Dynamic Style added

--- customReact/CustomReactRender.js
+++ customReact/CustomReactRender.js
@@ -1,33 +1,49 @@
 //Create Custom React Element Structure
 
 const reactElement = {
-    tagType: "a",
-    props:{
-        href: "https://react.dev/",
-        target: "_blank"
-    },
-    children: "Click here to learn React in detail"
-}
+  tagType: "a",
+  props: {
+    href: "https://react.dev/",
+    target: "_blank",
+  },
+  children: "Click here to learn React in detail",
+};
 
 // Create container in which above react element should be injected
 const customConatiner = document.querySelector("#root");
 
 // Create custom function which takes two arguments and render react element.
 
-function customRender(reactElement, container){
+function customeRenderInStaticStyle(reactElement, container) {
+  //Step-1: create document element for react tag type
+  const domElement = document.createElement(reactElement.tagType);
 
-    //Step-1: create document element for react tag type
-    const domElement = document.createElement(reactElement.tagType);
+  //Step-2: Add props attribute in domElement
+  domElement.setAttribute("href", reactElement.props["href"]); // One way to read value of Object
+  domElement.setAttribute("target", reactElement.props.target); // This another way to ready value of object
 
-    //Step-2: Add props attribute in domElement
-    domElement.setAttribute("href", reactElement.props['href']); // One way to read value of Object
-    domElement.setAttribute("target", reactElement.props.target); // This another way to ready value of object
+  //Step-3: Add children to dom element
+  domElement.innerHTML = reactElement.children;
 
-    //Step-3: Add children to dom element
-    domElement.innerHTML = reactElement.children;
-
-    //Final Step: add dom element in container
-    container.appendChild(domElement);
+  //Final Step: add dom element in container
+  container.appendChild(domElement);
 }
 
-customRender(reactElement, customConatiner);
+const customeRenderInDynamicStyle = (reactElement, container) => {
+  //Step-1: create document element for react tag type
+  const domElement = document.createElement(reactElement.tagType);
+
+  //Step-2: Loop over prop object to set attribute dynamically in dom element
+  Object.keys(reactElement.props).forEach((propKey) => {
+    domElement.setAttribute(propKey, reactElement.props[propKey]);
+  });
+
+  //Step-3: Add children to dom element
+  domElement.innerHTML = reactElement.children;
+
+  //Final Step: add dom element in container
+  container.appendChild(domElement);
+};
+
+//customeRenderInStaticStyle(reactElement, customConatiner);
+customeRenderInDynamicStyle(reactElement, customConatiner);
